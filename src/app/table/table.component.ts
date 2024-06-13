7567494561a840ffaf2fcfea4a0e7432819f373c
Igor Kos <ikos1801@gmail.com>
2024-06-13 15:12:04 +0200

    fixed table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,12 +1,10 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MatTable, MatTableModule} from '@angular/material/table';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
 import {EmissionService} from "../shared/emission.service";
 import {DailyEmissionModel} from "../auth/model/dailyEmission.model";
-import {ErrorHandlerUtil} from "../shared/error-handler.utils";
 import {CommonModule} from "@angular/common";
-import {TableDataSource} from "./table-datasource";
 
 @Component({
   selector: 'app-table',
@@ -15,35 +13,32 @@ import {TableDataSource} from "./table-datasource";
   standalone: true,
   imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule]
 })
-export class TableComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: TableDataSource;
-  dataSource: DailyEmissionModel[] = [];
+export class TableComponent implements OnInit {
 
-  displayedColumns = [
-    'date',
-    'transportationEmissionDailyTotal',
-    'utilityEmissionDailyTotal',
-    'foodEmissionDailyTotal',
-    'totalEmission'
-  ];
+  // @ts-ignore
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  // @ts-ignore
+  @ViewChild(MatSort) sort: MatSort;
+  // @ts-ignore
+  dataSource: MatTableDataSource<any>;
+
+  pageSize = 10;
+  pageSizeOptions = [10, 50, 100];
+  displayedColumns: string[] = ['date', 'transportationEmissionDailyTotal', 'utilityEmissionDailyTotal', 'foodEmissionDailyTotal', 'totalEmission'];
 
   constructor(private emissionService: EmissionService) {
   }
 
-  ngAfterViewInit(): void {
-    this.emissionService.getEmissions().subscribe(
-      {
-        next: (response: DailyEmissionModel[]) => {
-          this.dataSource = response;
-          this.table.data = this.dataSource;
-          this.table.sort = this.sort; // Set the MatSort on the table data source
-          this.table.paginator = this.paginator; // Set the MatPaginator on the table data source
-        },
-        error: err => {
-          ErrorHandlerUtil.handleError(err);
-        }
-      });
+  ngOnInit() {
+    this.emissionService.getEmissions().subscribe({
+      next: (response: DailyEmissionModel[]) => {
+        this.dataSource = new MatTableDataSource(response);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: err => {
+        console.error('Error fetching emissions', err);
+      }
+    });
   }
 }
